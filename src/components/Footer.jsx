2a5619f26b76aa0d8450b9dfd10136e29ad47f65
Sanjay555# <sanjaykandregula5@gmail.com>
2025-05-25 13:01:52 +0530

    Footer links changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, HashRouter as Router } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "../assets/webp/footer-logo.webp";
 import facebook from "../assets/webp/facebook.webp";
 import twitter from "../assets/webp/twitter.webp";
